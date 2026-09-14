2d7cffdd16bf53db416d8a413efb4db6c77adbbd
haifei <haifei802@gmail.com>
2026-09-14 21:18:30 +0800

    update a hello from the feature branch

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main()
 {
     // @TODO: print a sentence you want.
-    printf("Hello, Fudan University!\n");
+    printf("Hello, Fudan University in the feature branch!!\n");
     return 0;
 }
